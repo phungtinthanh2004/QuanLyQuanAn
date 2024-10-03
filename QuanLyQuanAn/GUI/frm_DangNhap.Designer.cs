c388a6a20a3837f9b7f342980af7f4478cd7c9b4
phungtinthanh2004 <phungtinthanh@gmail.com>
2024-10-03 21:30:16 +0700

    Thêm icon cho Form Đăng Nhập

--- QuanLyQuanAn/GUI/frm_DangNhap.Designer.cs
+++ QuanLyQuanAn/GUI/frm_DangNhap.Designer.cs
@@ -159,6 +159,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frm_DangNhap";
             this.Text = "Đăng nhập";
             this.ResumeLayout(false);
