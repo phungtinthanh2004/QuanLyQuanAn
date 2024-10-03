aadb357a4482874e2da42b4647e8e30226767826
phungtinthanh2004 <phungtinthanh@gmail.com>
2024-10-03 21:28:14 +0700

    Thiết kế Form Đăng Nhập

--- QuanLyQuanAn/GUI/frm_DangNhap.cs
+++ QuanLyQuanAn/GUI/frm_DangNhap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class frm_DangNhap : Form
     {
-        public Form1()
+        public frm_DangNhap()
         {
             InitializeComponent();
         }
